304d7c45fac48b15ae201e843c6f5828c794326b
Hermann <g4mak3r@gmail.com>
2026-09-17 23:02:16 +0300

    Expose BuildConfig version in release UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    buildFeatures { compose = true }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+    }
     packaging { resources.excludes += setOf("META-INF/LICENSE*", "META-INF/NOTICE*") }
 
     testOptions {
