becd6496f602845058e752abecf748f973390490
Hermann <g4mak3r@gmail.com>
2026-09-17 19:23:05 +0300

    Bump cubikSolver to 0.5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.cubecraft.solver"
         minSdk = 29
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.5.2"
+        versionCode = 13
+        versionName = "0.5.3"
         vectorDrawables { useSupportLibrary = true }
     }
 
