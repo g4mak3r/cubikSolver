6533bd3cdc6f826460966863e8b034c6659efba5
Hermann <g4mak3r@gmail.com>
2026-09-18 20:28:50 +0300

    Simplify scan route labels

--- app/src/main/java/com/cubecraft/solver/scanner/ScanPose.kt
+++ app/src/main/java/com/cubecraft/solver/scanner/ScanPose.kt
@@ -2,13 +2,16 @@ package com.cubecraft.solver.scanner
 
 import com.cubecraft.solver.model.Face
 
-data class ScanPose(val face: Face, val title: String, val instruction: String)
+data class ScanPose(
+    val face: Face,
+    val title: String
+)
 
 val scanSequence = listOf(
-    ScanPose(Face.F, "START WITH ANY FACE", ""),
-    ScanPose(Face.R, "TURN LEFT", ""),
-    ScanPose(Face.B, "TURN LEFT", ""),
-    ScanPose(Face.L, "TURN LEFT", ""),
-    ScanPose(Face.U, "RETURN TO {FRONT_CENTER} CENTER", "TILT DOWN"),
-    ScanPose(Face.D, "RETURN TO {FRONT_CENTER} CENTER", "TILT UP")
+    ScanPose(Face.F, "ANY FACE"),
+    ScanPose(Face.R, "TURN LEFT"),
+    ScanPose(Face.B, "TURN LEFT"),
+    ScanPose(Face.L, "TURN LEFT"),
+    ScanPose(Face.U, "FIND TARGET"),
+    ScanPose(Face.D, "FIND TARGET")
 )
