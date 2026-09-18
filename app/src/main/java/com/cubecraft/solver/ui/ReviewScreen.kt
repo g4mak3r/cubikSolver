64ac3b25e896208589b8d92743eef995372e0f04
Hermann <g4mak3r@gmail.com>
2026-09-18 18:29:54 +0300

    Fix luminance imports in retro UI

--- app/src/main/java/com/cubecraft/solver/ui/ReviewScreen.kt
+++ app/src/main/java/com/cubecraft/solver/ui/ReviewScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
