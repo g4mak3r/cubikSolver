6550a3761af9be7f6ce046cbdf3d0198788a982d
Hermann <g4mak3r@gmail.com>
2026-09-18 21:55:44 +0300

    Bump cubikSolver to 0.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.cubecraft.solver"
         minSdk = 29
         targetSdk = 35
-        versionCode = 25
-        versionName = "0.8.3"
+        versionCode = 26
+        versionName = "0.9.0"
         vectorDrawables { useSupportLibrary = true }
         ndk {
             abiFilters += listOf("arm64-v8a")
