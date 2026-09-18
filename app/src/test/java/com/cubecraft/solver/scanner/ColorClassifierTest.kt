c95864bd273e132b2b1a1bb618e8e37b12a701a1
Hermann <g4mak3r@gmail.com>
2026-09-18 20:06:05 +0300

    Test scan palette is always canonical

--- app/src/test/java/com/cubecraft/solver/scanner/ColorClassifierTest.kt
+++ app/src/test/java/com/cubecraft/solver/scanner/ColorClassifierTest.kt
@@ -29,4 +29,36 @@ class ColorClassifierTest {
         val orangeRef = sample(RgbColor(255, 95, 5))
         assertTrue(cubeColorDistance(red, redRef) < cubeColorDistance(red, orangeRef))
     }
+
+
+    @Test
+    fun balancedClassifierAlwaysExposesAllSixCanonicalColors() {
+        val physical = mapOf(
+            com.cubecraft.solver.model.Face.U to StickerGuess.WHITE,
+            com.cubecraft.solver.model.Face.R to StickerGuess.RED,
+            com.cubecraft.solver.model.Face.F to StickerGuess.GREEN,
+            com.cubecraft.solver.model.Face.D to StickerGuess.YELLOW,
+            com.cubecraft.solver.model.Face.L to StickerGuess.ORANGE,
+            com.cubecraft.solver.model.Face.B to StickerGuess.BLUE
+        )
+        val captures = com.cubecraft.solver.model.Face.entries.map { face ->
+            val color = physical.getValue(face)
+            val canonical = canonicalColorSample(color)
+            CapturedFace(
+                face = face,
+                samples = List(9) { canonical },
+                quality = 1f,
+                guesses = List(9) { StickerGuess.RED }
+            )
+        }
+
+        val result = BalancedClassifier.classify(captures, 3)
+
+        assertTrue(result.colorFaces.keys.containsAll(canonicalStickerGuesses))
+        assertTrue(result.colorFaces.size == 6)
+        assertTrue(
+            result.palette.values.toSet() ==
+                canonicalStickerGuesses.map(::idealDisplayRgb).toSet()
+        )
+    }
 }
