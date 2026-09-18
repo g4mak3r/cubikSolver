5d4a95e8adda3ee413b630c59e2ca1a695065ec4
Hermann <g4mak3r@gmail.com>
2026-09-18 18:44:00 +0300

    Add harder random-like 5x5 state regression

--- app/src/test/java/com/cubecraft/solver/solver/FiveByFiveSolverTest.kt
+++ app/src/test/java/com/cubecraft/solver/solver/FiveByFiveSolverTest.kt
@@ -24,6 +24,10 @@ class FiveByFiveSolverTest {
         assertStickerStateSolve("Rw U2 Fw' R2 D Lw B U' Rw2 F")
     }
 
+    @Test fun solvesRandomLikeWideScrambleFromStateOnly() {
+        assertStickerStateSolve("Rw U Fw2 D' Lw B2 Uw R2 F' Dw L2 Bw' U2 Rw'")
+    }
+
     private fun assertStickerStateSolve(scramble: String) {
         val scannedState = CubeState(5)
         scannedState.applyAll(Move.parseAlgorithm(scramble))
